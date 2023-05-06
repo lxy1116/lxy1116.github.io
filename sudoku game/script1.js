e7677c489bd5dea151d67eb153f6e61ab31e8c45
lxy1116 <126554149+lxy1116@users.noreply.github.com>
2023-05-06 15:41:10 +0800

    Add files via upload

---
+++ sudoku game/script1.js
@@ -0,0 +1,239 @@
+function numberA(){
+  var response=prompt("Please enter the right number. A=");
+  if(response=="3"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberB(){
+  var response=prompt("Please enter the right number. B=");
+  if(response=="1"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberC(){
+  var response=prompt("Please enter the right number. C=");
+  if(response=="7"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberD(){
+  var response=prompt("Please enter the right number. D=");
+  if(response=="3"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberE(){
+  var response=prompt("Please enter the right number. E=");
+  if(response=="3"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberF(){
+  var response=prompt("Please enter the right number. F=");
+  if(response=="1"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberG(){
+  var response=prompt("Please enter the right number. G=");
+  if(response=="5"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberH(){
+  var response=prompt("Please enter the right number. H=");
+  if(response=="4"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberI(){
+  var response=prompt("Please enter the right number. I=");
+  if(response=="5"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberJ(){
+  var response=prompt("Please enter the right number. J=");
+  if(response=="6"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberK(){
+  var response=prompt("Please enter the right number. K=");
+  if(response=="9"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberL(){
+  var response=prompt("Please enter the right number. L=");
+  if(response=="6"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberM(){
+  var response=prompt("Please enter the right number. M=");
+  if(response=="2"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberN(){
+  var response=prompt("Please enter the right number. N=");
+  if(response=="3"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberO(){
+  var response=prompt("Please enter the right number. O=");
+  if(response=="9"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberP(){
+  var response=prompt("Please enter the right number. P=");
+  if(response=="8"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberQ(){
+  var response=prompt("Please enter the right number. Q=");
+  if(response=="3"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberR(){
+  var response=prompt("Please enter the right number. R=");
+  if(response=="4"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberS(){
+  var response=prompt("Please enter the right number. S=");
+  if(response=="2"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberT(){
+  var response=prompt("Please enter the right number. T=");
+  if(response=="8"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberU(){
+  var response=prompt("Please enter the right number. U=");
+  if(response=="2"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberV(){
+  var response=prompt("Please enter the right number. V=");
+  if(response=="4"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberW(){
+  var response=prompt("Please enter the right number. W=");
+  if(response=="8"){
+    alert("Right answer! Next blank");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
+
+function numberX(){
+  var response=prompt("Please enter the right number. X=");
+  if(response=="5"){
+    alert("Right answer! You win! Congratulations!");
+  }
+  else {
+    alert("Wrong answer!");
+  }
+}
